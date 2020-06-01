49f2a20f67911ab643f081a022a4af317ad6b614
Paul Finegan <paul.s.finegan@gmail.com>
2020-06-01 13:56:28 +1000

    modified error response, modified styles

--- routes/newURL.js
+++ routes/newURL.js
@@ -4,13 +4,16 @@ const dns = require('dns');
 const {shortUrlModel,getLastUrl,getMatchingUrl,createNewShortUrl} = require('../appFunctions');
 //this router handles the creation of a new short URL.
 router.post("/new",(req,res,next)=>{
+	//get the url, checl it is in valid format, then carry out a dns lookup to see if exists.  if it doesnt pass these tests, return invalid.
+	//if it does, then we check to see if it has been previously entered and return the previous entry, if not we check the last added value, increment it, add to the database and return the entry to the user.
 	url = req.body.url;
 	const nsLookup = url.match(/[\w\.-]+(?:\.[\w\.-]+)/g)[0]
 	if (url.match(/^(?:http(s)?:\/\/)?[\w\.-]+(?:\.[\w\.-]+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.])+$/g)){
 			dns.lookup( nsLookup,(err,resp)=> {err ? res.json({"error":"invalid url"}) : 
-				getMatchingUrl(url,(err,data)=>{err ? res.json({"error":"an error has occured"}) : data ? next(res.json({"original_url":data.url,"short_url":data.shortUrl})) :
+				getMatchingUrl(url,(err,data)=>{err ? 
+					next(err) : data ? next(res.json({"original_url":data.url,"short_url":data.shortUrl})) :
 				getLastUrl((err,data)=>{ err ? console.log(err) : 
-					createNewShortUrl(url,++data[0].shortUrl,(err,data)=>{err ? res.json({"error":"an error has occured"}) : next(res.json({"original_url":data.url,"short_url":data.shortUrl}))})
+					createNewShortUrl(url,++data[0].shortUrl,(err,data)=>{err ? next(err) : next(res.json({"original_url":data.url,"short_url":data.shortUrl}))})
 				})})
 	})} 
 	else{
